f989f2864e29f15d94f521d9c81d245ab3d154f6
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-03 10:16:00 -0700

    add landing component to repo

---
+++ js/Landing.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Landing = () =>
+  <div className="landing">
+    <h1>Matt's Video Service</h1>
+    <input type="text" placeholder="search" />
+    <a href="#">or Browse All</a>
+  </div>
+
+export default Landing;
