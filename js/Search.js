9a3b8bb962a62427d10280349b250ebc6bdba0f0
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-03 11:25:57 -0700

    create markup for search component

--- js/Search.js
+++ js/Search.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import preload from '../data';
-
+console.log(preload);
 const Search = () =>
   <div className="search">
-    {preload.shows.map((show => <h3>{show.title}</h3>))}
+    {preload.shows.map((show =>
+      <div>
+        <div className="show-card">
+          <img alt={`${show.title} Show Poster`} src={`/img/posters/${show.poster}`} />
+        </div>
+        <div>
+          <h3>{show.title}</h3>
+          <h4>({show.year})</h4>
+          <p>{show.description}</p>
+        </div>
+      </div>
+    ))}
   </div>
 
  export default Search;
