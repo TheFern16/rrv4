50e8df1d2e2b095956d2d48bcb599807f01ab23a
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-03 11:09:11 -0700

    import json data

--- js/Search.js
+++ js/Search.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import preload from '../data';
 
 const Search = () =>
-  <h1>Search Bar</h1>
+  <div className="search">
+    <pre>
+      <code>
+        {JSON.stringify(preload, null, 4)}
+      </code>
+    </pre>
+  </div>
 
  export default Search;
