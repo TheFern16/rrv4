37f07db752eec3c3a8c343de04dacff77e195006
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-04 10:29:45 -0700

    change search to a class component

--- js/Search.js
+++ js/Search.js
@@ -4,17 +4,25 @@ import preload from '../data';
 // components
 import ShowCard from './ShowCard';
 
-const Search = () =>
-  <div className="search">
-    <header>
-      <h1>svideo</h1>
-      <input type="text" placeholder="Search" />
-    </header>
-    <div>
-      {preload.shows.map((show, key) =>
-        <ShowCard show={show} key={show.imdbID} index={key} />
-      )}
-    </div>
-  </div>
+class Search extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input type="text" placeholder="Search" />
+        </header>
+        <div>
+          {preload.shows.map((show, key) =>
+            <ShowCard show={show} key={show.imdbID} index={key} />
+          )}
+        </div>
+      </div>
+    )
+  }
+}
 
  export default Search;
