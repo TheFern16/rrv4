5be559b5745cf5864bf6bd8176988252521975c8
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-05 10:42:40 -0700

    add filter component test case to jest suite

--- js/__tests__/Search.spec.js
+++ js/__tests__/Search.spec.js
@@ -14,11 +14,23 @@ describe('Search', () => {
 
   it('should render correct all of the shows', () => {
     const component = shallow(<Search />);
-    expect(preload.shows.length).toEqual(component.find(ShowCard).length)
+    expect(component.find(ShowCard).length).toEqual(preload.shows.length);
   });
 
-  xit('should render correct number of shows based on search', () => {
+  it('should render correct number of shows based on search', () => {
+    const searchWord = 'black';
+    const component = shallow(<Search />);
+    component.find('input').simulate('change', {
+      target: {
+        value: searchWord
+      }
+    });
 
+    const showCount = preload.shows.filter(show => `${show.title} ${show.description}`
+      .toUpperCase()
+      .indexOf(searchWord.toUpperCase()) >= 0).length;
+
+    expect(component.find(ShowCard).length).toEqual(showCount);
   });
 })
 
