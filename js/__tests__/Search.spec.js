5ca08f7e77c61e8b58acfc13052696ddfcf8feb7
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-05 10:03:19 -0700

    add enzyme and refactor test case for enzyme

--- js/__tests__/Search.spec.js
+++ js/__tests__/Search.spec.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import Search from '../Search';
 
 test('Search renders correctly', () => {
-  const component = renderer.create(<Search />);
-  const tree = component.toJSON();
+  const component = shallow(<Search />);
   expect(tree).toMatchSnapshot();
 });
