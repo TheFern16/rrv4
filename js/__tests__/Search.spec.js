44a978a3183d3ee78dcc107131f37b8336db010d
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-05 10:26:13 -0700

    complete second test and refactor for describe

--- js/__tests__/Search.spec.js
+++ js/__tests__/Search.spec.js
@@ -6,16 +6,19 @@ import preload from '../../data.json';
 import Search from '../Search';
 import ShowCard from '../ShowCard';
 
-test('Search renders correctly', () => {
-  const component = shallow(<Search />);
-  expect(component).toMatchSnapshot();
-});
+describe('Search', () => {
+  it('renders correctly', () => {
+    const component = shallow(<Search />);
+    expect(component).toMatchSnapshot();
+  });
 
-test('Search should render correct all of the shows', () => {
-  const component = shallow(<Search />);
-  expect(preload.shows.length).toEqual(component.find(ShowCard).legth);
-});
+  it('should render correct all of the shows', () => {
+    const component = shallow(<Search />);
+    expect(preload.shows.length).toEqual(component.find(ShowCard).length)
+  });
 
-test('Search should render correct number of shows based on search', () => {
+  xit('should render correct number of shows based on search', () => {
 
-});
+  });
+})
+
