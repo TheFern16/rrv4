bc4f32d72a278720c44ab6ffe1520d1fe8daa494
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-05 10:20:59 -0700

    add test to check if all shows render

--- js/__tests__/Search.spec.js
+++ js/__tests__/Search.spec.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import preload from '../../data.json';
+
+// components
 import Search from '../Search';
+import ShowCard from '../ShowCard';
 
 test('Search renders correctly', () => {
   const component = shallow(<Search />);
   expect(component).toMatchSnapshot();
+});
+
+test('Search should render correct all of the shows', () => {
+  const component = shallow(<Search />);
+  expect(preload.shows.length).toEqual(component.find(ShowCard).legth);
+});
+
+test('Search should render correct number of shows based on search', () => {
+
 });
