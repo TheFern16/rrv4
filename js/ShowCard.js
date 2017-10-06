fcab92810f6658c1a615a617ec5d13cdd4ae9821
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-06 14:52:09 -0700

    fix show card render

--- js/ShowCard.js
+++ js/ShowCard.js
@@ -1,16 +1,19 @@
 // @flow
-
 import React from 'react';
+
 // import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const Wrapper = styled.div`
+const Wrapper = styled(Link)`
   width: 32%;
   border: 2px solid #333;
   border-radius: 4px;
   margin-bottom: 25px;
   padding-right: 10px;
   overflow: hidden;
+  color: black;
+  text-decoration: none;
 `;
 
 const Image = styled.img`
@@ -19,27 +22,19 @@ const Image = styled.img`
   margin-right: 10px;
 `;
 
-
-const ShowCard = (props: {
-    description: PropTypes.string,
-    imdbID: PropTypes.string,
-    poster: PropTypes.string,
-    title: PropTypes.string,
-    trailer: PropTypes.string,
-    year: PropTypes.string
-  }) => {
-    return (
-      <Wrapper>
-        <Image
-          alt={`${props.show.title} Show Poster`}
-          src={`/img/posters/${props.show.poster}`} />
-        <div>
-          <h3>{props.show.title}</h3>
-          <h4>({props.show.year})</h4>
-          <p>{props.show.description}</p>
-        </div>
-      </Wrapper>
-    )
+const ShowCard = (props: Show) => {
+  return (
+    <Wrapper to={`/details/${props.show.imdbID}`}>
+      <Image
+        alt={`${props.show.title} Show Poster`}
+        src={`/img/posters/${props.show.poster}`} />
+      <div>
+        <h3>{props.show.title}</h3>
+        <h4>({props.show.year})</h4>
+        <p>{props.show.description}</p>
+      </div>
+    </Wrapper>
+  )
 }
 
 // ShowCard.propTypes = {
