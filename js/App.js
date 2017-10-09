be8b2be5cbaa56cd16bfd8216d98f0e671ccab85
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-09 11:05:51 -0700

    import store and provider into main app component

--- js/App.js
+++ js/App.js
@@ -3,6 +3,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import type { Match } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 // data
 import preload from '../data.json';
@@ -16,22 +18,24 @@ const FourZeroFour = () => <h1>Page Not Found</h1>;
 
 const App = () =>
   <BrowserRouter>
-    <div className="app">
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route path="/search" component={ props => <Search shows={preload.shows} />} />
-        <Route
-          path="/details/:id"
-          component={
-            (props: { match: Match }) => {
-              const sShow = preload.shows.find(show => props.match.params.id === show.imdbID);
-              return <Details show={sShow} {...props} />
+    <Provider store={store}>
+      <div className="app">
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/search" component={ props => <Search shows={preload.shows} />} />
+          <Route
+            path="/details/:id"
+            component={
+              (props: { match: Match }) => {
+                const sShow = preload.shows.find(show => props.match.params.id === show.imdbID);
+                return <Details show={sShow} {...props} />
+              }
             }
-          }
-        />
-        <Route component={FourZeroFour} />
-      </Switch>
-    </div>
+          />
+          <Route component={FourZeroFour} />
+        </Switch>
+      </div>
+    </Provider>
   </BrowserRouter>
 
 export default App;
