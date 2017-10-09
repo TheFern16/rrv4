7959d2805a33d687fd0131bc88b0cbc937d3f4f2
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-09 11:10:05 -0700

    add map state to props to landing comp

--- js/Landing.js
+++ js/Landing.js
@@ -1,6 +1,7 @@
 // @flow
 
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const Landing = () =>
@@ -10,4 +11,6 @@ const Landing = () =>
     <Link to="/search">or Browse All</Link>
   </div>
 
-export default Landing;
+const mapStateToProps = (state) => ({ searchTerm: state.searchTerm });
+
+export default connect(mapStateToProps)(Landing);
