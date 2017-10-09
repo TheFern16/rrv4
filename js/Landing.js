93e9d965fdb323fb26060baf528dc8772db8cc57
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-09 11:23:42 -0700

    use mapdispatch to props in landing component

--- js/Landing.js
+++ js/Landing.js
@@ -3,14 +3,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import setSearchTerm from './actionCreators';
 
-const Landing = () =>
+const Landing = (props: { searchTerm: string, handleSearchTermChange: Function }) => (
   <div className="landing">
     <h1>Matt's Video Service</h1>
-    <input type="text" placeholder="search" />
+    <input onChange={props.handleSearchTermChange}value={props.searchTerm} type="text" placeholder="search" />
     <Link to="/search">or Browse All</Link>
   </div>
+);
 
-const mapStateToProps = (state) => ({ searchTerm: state.searchTerm });
+const mapStateToProps = state => ({ searchTerm: state.searchTerm });
+const mapDispatchToProps = (dispatch: Function) => ({
+  handleSearchTermChange(event) {
+    dispatch(setSearchTerm(event.target.value));
+  }
+});
 
-export default connect(mapStateToProps)(Landing);
+export default connect(mapStateToProps, mapDispatchToProps)(Landing);
