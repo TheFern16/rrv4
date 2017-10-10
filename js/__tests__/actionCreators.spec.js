7e6331800b7a40e54461c9c4dcc55075374b2967
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-10 14:04:07 -0700

    add test for should call apiDataData and pull show from correct api using thunk

--- js/__tests__/actionCreators.spec.js
+++ js/__tests__/actionCreators.spec.js
@@ -1,6 +1,17 @@
 // @flow
 
-import { setSearchTerm, addApiData } from '../actionCreators';
+import moxios from 'moxios';
+import { setSearchTerm, addApiData, getApiDetails } from '../actionCreators';
+
+const strangerThings = {
+  "title": "Stranger Things",
+  "year": "2016–",
+  "description": "When a young boy disappears, his mother, a police chief, and his friends must confront terrifying forces in order to get him back.",
+  "poster": "st.jpg",
+  "imdbID": "tt4574334",
+  "trailer": "9Egf5U8xLo8",
+  "rating": "8.6"
+}
 
 describe('actionCreators', () => {
 
@@ -8,22 +19,28 @@ describe('actionCreators', () => {
     expect(setSearchTerm('New York')).toMatchSnapshot();
   });
 
-  it('should render results for a black query', () => {
-    expect(setSearchTerm('black')).toMatchSnapshot();
+  it('should add api data when passed an api data object', () => {
+    expect(addApiData(strangerThings)).toMatchSnapshot();
   });
 
-  it('should add api data when passed an api data object', () => {
-    expect(
-      addApiData({
-        "title": "Stranger Things",
-        "year": "2016–",
-        "description": "When a young boy disappears, his mother, a police chief, and his friends must confront terrifying forces in order to get him back.",
-        "poster": "st.jpg",
-        "imdbID": "tt4574334",
-        "trailer": "9Egf5U8xLo8",
-        "rating": "8.6"
-      })
-    ).toMatchSnapshot();
+  it('should call addApiData, and pull show from correct api using the thunk', (done: Function) => {
+    const dispatchMock = jest.fn();
+    moxios.withMock(() => {
+      getApiDetails(strangerThings.imdbID)(dispatchMock);
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent();
+        request.respondWith({
+          status: 200,
+          response: strangerThings
+        })
+        .then(() => {
+          expect(request.url).toEqual(`http://localhost:3000/${strangerThings.imdbID}`);
+          expect(dispatchMock).toBeCalledWith(addApiData(strangerThings));
+          done();
+        });
+      });
+    });
   });
+
 });
 
