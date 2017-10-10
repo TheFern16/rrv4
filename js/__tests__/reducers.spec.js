2dc05ae7bb204ce5e0795ade90e318fd850625ae
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-10 11:58:43 -0700

    add test case for west as a search term

--- js/__tests__/reducers.spec.js
+++ js/__tests__/reducers.spec.js
@@ -1,8 +1,14 @@
 import reducers from '../reducers';
 
 describe('SET_SEARCH_TERM', () => {
+
   it('given the input of black, state should equal the redux state search', () => {
-    let state = reducers({ searchTerm:'', apiData:{} }, { type:'SET_SEARCH_TERM',payload:'black' });
-    expect(state).toEqual({ searchTerm:'black', apiData:{}});
+    let state = reducers({ searchTerm:'', apiData:{} }, { type:'SET_SEARCH_TERM', payload:'black' });
+    expect(state).toEqual({ searchTerm: 'black', apiData:{} });
+  });
+
+  it ('should have a length of two when user inputs west into the search bar', () => {
+    let state = reducers({ searchTerm: '', apiData: {} }, { type: 'SET_SEARCH_TERM', payload: 'west' });
+    expect(state).toEqual({ searchTerm: 'west', apiData: {} });
   });
 });
