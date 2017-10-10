246850445c64601bdbd14113ad05b6dc72e64297
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-10 12:25:47 -0700

    desc should equal user interaction with the api

--- js/__tests__/reducers.spec.js
+++ js/__tests__/reducers.spec.js
@@ -3,12 +3,18 @@ import reducers from '../reducers';
 describe('SET_SEARCH_TERM', () => {
 
   it('given the input of black, state should equal the redux state search', () => {
-    let state = reducers({ searchTerm:'', apiData:{} }, { type:'SET_SEARCH_TERM', payload:'black' });
+    const state = reducers({ searchTerm:'', apiData:{} }, { type:'SET_SEARCH_TERM', payload:'black' });
     expect(state).toEqual({ searchTerm: 'black', apiData:{} });
   });
 
   it ('should have a length of two when user inputs west into the search bar', () => {
-    let state = reducers({ searchTerm: '', apiData: {} }, { type: 'SET_SEARCH_TERM', payload: 'west' });
+    const state = reducers({ searchTerm: '', apiData: {} }, { type: 'SET_SEARCH_TERM', payload: 'west' });
     expect(state).toEqual({ searchTerm: 'west', apiData: {} });
   });
+
+  it('api data should be equal to queries clicks made on the desc by the user', () => {
+    const state = reducers({ searchTerm:'',apiData:{} }, { type:'ADD_API_DATA', payload: {rating:'2.3', title:'House of Cards', year:'2013–', description: 'A Congressman works with his equally conniving wife to exact revenge on the people who betrayed him.',poster: 'hoc.jpg', imdbID:'tt1856010', trailer: 'NTzycsqxYJ0'}});
+    expect(state).toEqual({ searchTerm:'', apiData: { tt1856010: { rating:'2.3', title:'House of Cards', year: '2013–', description: 'A Congressman works with his equally conniving wife to exact revenge on the people who betrayed him.', poster: 'hoc.jpg',imdbID: 'tt1856010', trailer: 'NTzycsqxYJ0'}}});
+  });
 });
+
