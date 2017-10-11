873bac4fdc8b6c6e05f438b091c086e9d362d4e2
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-11 10:56:28 -0700

    bring in requirements for express server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 require('babel-register');
+
+const express = require('express');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+const ReactRouter = require('react-router-dom');
+const _ = require('lodash');
+const fs = require('fs');
+const App = require('./js/App').default;
