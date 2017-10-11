1e42dd9361b541dbb14ce9ac45fa7a6deb07e377
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-11 11:57:54 -0700

    create async route component

---
+++ js/AsyncRoute.js
@@ -0,0 +1,14 @@
+// @flow
+
+import React from 'react';
+import Spinner from './Spinner';
+
+class AsyncRoute extends React.Component {
+  render() {
+    return (
+      <h1>High Order Component</h1>
+    )
+  }
+}
+
+export class AsyncRoute;
