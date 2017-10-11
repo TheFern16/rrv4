61b2addb3fee91eac6a5c3775b2616149792d7bf
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-11 10:54:35 -0700

    add es2015 babel comp for server env

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,11 +1,10 @@
 // @flow
+
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 // components
-import Landing from './Landing';
-import Search from './Search';
 import App from './App';
 
 const renderApp = () => {
